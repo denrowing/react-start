19853758f358363d37abce4b26edb366937d3e27
Denys Burdin <denrowing@gmail.com>
2021-08-10 23:54:34 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import './App.css'
-import Product from './components/Product'
-
 export default function App() {
-
   return (
-    <div className="products">
-     <Product title={'milk'} price={'25 uah'}/>
-     <Product title={'juice'} price={'120 uah'}/>
-        <Product title={'bread'} price={'40 uah'}/>
+    <div>
+    App
+
     </div>
   );
 }
-
-
