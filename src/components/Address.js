1875b1c2bcf4a0e2de9b64d8bc4637ee57edcc03
Denys Burdin <denrowing@gmail.com>
2021-08-11 02:50:16 +0300

    init les1

--- src/components/Address.js
+++ src/components/Address.js
@@ -3,14 +3,17 @@ import Geo from "./Geo";
 export default function Address(props) {
   let {street, suite, city, zipcode, geo} = props
   return (
-    <div className={'address'}>
-      <p>street: {street},</p>
-      <p>suite: {suite},</p>
-      <p>city: {city},</p>
-      <p>zipcode: {zipcode},</p>
-      <p>geo: {geo}</p>
-            <Geo lat={'-37.3159'} lng={'81.1496'}/>
-            <Geo lat={'-43.9509'} lng={'-34.4618'}/>
+    <div className={'addresses'}>
+        <div className={'address1'}>
+           <p>street: {street},</p>
+           <p>suite: {suite},</p>
+           <p>city: {city},</p>
+           <p>zipcode: {zipcode},</p>
+           <p>geo: {geo}</p>
+              <Geo lat={'-37.3159'} lng={'81.1496'}/>
+              <Geo lat={'-43.9509'} lng={'-34.4618'}/>
+        </div>
+
     </div>
   );
 }
