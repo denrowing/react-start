a643119c1520b5791dd0b50f31299b64c2a0cdfb
Denys Burdin <denrowing@gmail.com>
2021-08-27 11:51:16 +0300

    init commit 5

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import {
+  BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    withRouter
+} from "react-router-dom";
+import Users from "./components/Users";
+import './App.css'
+
 export default function App() {
   return (
-    <div>
-    App
+      <Router>
+          <div>
+              <div className={'wrap'}>
+                  <Link to={'/'}>to Home Page</Link>
+                  <Link to={'/users-page'}>to users Page</Link>
+              </div>
 
-    </div>
+            <Route path={'/users-page'} component={Users}/>
+
+          </div>
+      </Router>
   );
 }
+
+// Є три лінки
+// /users-page
+// /posts-page
+// /comments-page
+// При кліку на відповідну лінку відбувається перехід на відповідний компонент,
+//     який дістає з jsonplaceholder інформацію про відповідні стуності, та виводить їх повний список
---
+++ src/components/Address.js
@@ -0,0 +1,8 @@
+export default function Address() {
+  return (
+    <div>
+    Address
+
+    </div>
+  );
+}
