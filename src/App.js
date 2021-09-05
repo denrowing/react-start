726e4d71c3b9f4ba2fb6999e15d1fecc34e273bb
Denys Burdin <denrowing@gmail.com>
2021-09-06 01:40:29 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
+import {useDispatch, useSelector} from "react-redux";
+
+const SomeComponent = () => {
+  const counters = useSelector(({counterValue}) => counterValue)
+    const dispatch = useDispatch()
+
+  return (
+      <header>
+        <h1>{counters}</h1>
+        <button onClick={() => {
+            dispatch({type: 'INC A'})
+        }}>inc A</button>
+          <button onClick={() => {
+              dispatch({type: 'DEC A'})
+          }}>dec A</button>
+          <button onClick={() => {
+              dispatch({type: 'INC B'})
+          }}>inc B</button>
+          <button onClick={() => {
+              dispatch({type: 'DEC B'})
+          }}>dec B</button>
+          <button onClick={() => {
+              dispatch({type: 'INC C'})
+          }}>inc C</button>
+          <button onClick={() => {
+              dispatch({type: 'DEC C'})
+          }}>dec C</button>
+      </header>
+  )
+}
+
 export default function App() {
+
   return (
     <div>
-    App
+      <SomeComponent/>
 
     </div>
   );
 }
+
+// стоврити об'єкт {a:0,b:0,c:0}
+// Стоврити 6 кнопок.
+//     по дві на кожен з параметрів об'єкту.
+// одна кнопка збільшує значення , інша зменшує
+// логіку реалізувати через reducer
--- src/index.js
+++ src/index.js
@@ -4,9 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from 'redux'
+import {Provider} from "react-redux";
+
+const initialState = {
+    counterValue: {a:0,b:0,c:0}
+}
+
+const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC A': {
+            return {...state, counterValue: state.counterValue.a + 1}
+        }
+        case 'DEC A': {
+            return {...state, counterValue: state.counterValue.a - 1}
+        }
+        case 'INC B': {
+            return {...state, counterValue: state.counterValue.b + 2}
+        }
+        case 'DEC B': {
+            return {...state, counterValue: state.counterValue.b - 2}
+        }
+        case 'INC C': {
+            return {...state, counterValue: state.counterValue.c + 5}
+        }
+        case 'DEC C': {
+            return {...state, counterValue: state.counterValue.c - 5}
+        }
+        default:
+            return state
+    }
+}
+
+let store = createStore(counterReducer)
+
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={store}>
     <App />
+          </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
